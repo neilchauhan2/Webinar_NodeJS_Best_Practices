e0c8c2f0394c3070d74752f9259c5973af51392d
Neil Chauhan <neilchauhan2@gmail.com>
2018-11-02 19:09:59 +0530

    added Css framework

--- src/routes/pages/articles.js
+++ src/routes/pages/articles.js
@@ -1,29 +1,36 @@
-const route = require('express').Router()
-const { fetchArticles, fetchArticleById } = require('../../controllers/articles')
+const route = require("express").Router();
+const {
+  fetchArticles,
+  fetchArticleById
+} = require("../../controllers/articles");
 
-route.get('/', async (req, res) => {
+route.get("/add", (req, res) => {
+  res.render("pages/addArticle");
+});
+
+route.get("/", async (req, res) => {
   try {
-    const articles = await fetchArticles()
-    res.render('pages/articles', { articles })
+    const articles = await fetchArticles();
+    res.render("pages/articles", { articles });
   } catch (e) {
-    console.error(e)
-    res.redirect('/')
+    console.error(e);
+    res.redirect("/");
   }
-})
+});
 
-route.get('/:id', async (req, res) => {
+route.get("/:id", async (req, res) => {
   // Fetch a particular article
-  const articleId = req.params.id
+  const articleId = req.params.id;
   if (isNaN(parseInt(articleId))) {
-    console.error(new Error('Article ID is not correct number'))
-    res.redirect('/')
+    console.error(new Error("Article ID is not correct number"));
+    res.redirect("/");
   }
   try {
-    const article = await fetchArticleById(articleId)
-    res.render('pages/articles/id', { article })
+    const article = await fetchArticleById(articleId);
+    res.render("pages/articles/id", { article });
   } catch (e) {
-    throw e
+    throw e;
   }
-})
+});
 
-module.exports = route
+module.exports = route;
